5e6a250a27222f25ab23865525fa3e6c8036ce62
Don Wamutu <163501912+DonWamutu@users.noreply.github.com>
2026-08-03 23:06:11 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -16,42 +16,218 @@
    ============================================================ */
 
 const DEFAULT_DATA = {
-  grandparents: {
-    father: { id: "gp-father", name: "Grandfather's Name", image: null },
-    mother: { id: "gp-mother", name: "Grandmother's Name", image: null }
-  },
-  children: [
-    { id: "c1",  name: "First Child",   image: null, children: [
-      { id: "c1-1", name: "Grandchild", image: null },
-      { id: "c1-2", name: "Grandchild", image: null }
-    ]},
-    { id: "c2",  name: "Second Child",  image: null, children: [
-      { id: "c2-1", name: "Grandchild", image: null }
-    ]},
-    { id: "c3",  name: "Third Child",   image: null, children: [
-      { id: "c3-1", name: "Grandchild", image: null },
-      { id: "c3-2", name: "Grandchild", image: null }
-    ]},
-    { id: "c4",  name: "Fourth Child",  image: null, children: [
-      { id: "c4-1", name: "Grandchild", image: null }
-    ]},
-    { id: "c5",  name: "Fifth Child",   image: null, children: [
-      { id: "c5-1", name: "Grandchild", image: null }
-    ]},
-    { id: "c6",  name: "Sixth Child",   image: null, children: [
-      { id: "c6-1", name: "Grandchild", image: null }
-    ]},
-    { id: "c7",  name: "Seventh Child", image: null, children: [
-      { id: "c7-1", name: "Grandchild", image: null }
-    ]},
-    { id: "c8",  name: "Eighth Child",  image: null, children: [
-      { id: "c8-1", name: "Grandchild", image: null }
-    ]},
-    { id: "c9",  name: "Ninth Child",   image: null, children: [
-      { id: "c9-1", name: "Grandchild", image: null }
-    ]},
-    { id: "c10", name: "Tenth Child",   image: null, children: [
-      { id: "c10-1", name: "Grandchild", image: null }
-    ]}
-  ]
-};
+    "grandparents": {
+      "father": {
+        "id": "gp-father",
+        "name": "John Wamutu",
+        "image": null
+      },
+      "mother": {
+        "id": "gp-mother",
+        "name": "Monicah Nyakairu",
+        "image": null
+      }
+    },
+    "children": [
+      {
+        "id": "c1",
+        "name": "Magdalene Wangari Gitau",
+        "image": null,
+        "children": [
+          {
+            "id": "c1-1",
+            "name": "Nyaguthi",
+            "image": null
+          },
+          {
+            "id": "c1-2",
+            "name": "Wangui",
+            "image": null
+          },
+          {
+            "id": "p-4i3byjj",
+            "name": "Wambui",
+            "image": null
+          },
+          {
+            "id": "p-eqqvrhs",
+            "name": "Wanjiru",
+            "image": null
+          },
+          {
+            "id": "p-wljjvrd",
+            "name": "Maara",
+            "image": null
+          }
+        ]
+      },
+      {
+        "id": "c2",
+        "name": "Anne Wanjiru Kuria",
+        "image": null,
+        "children": [
+          {
+            "id": "c2-1",
+            "name": "Wanjohi",
+            "image": null
+          },
+          {
+            "id": "p-rymij55",
+            "name": "Wamutu",
+            "image": null
+          },
+          {
+            "id": "p-0wvz9is",
+            "name": "Muniu",
+            "image": null
+          }
+        ]
+      },
+      {
+        "id": "c3",
+        "name": "Paul Kimaru",
+        "image": null,
+        "children": [
+          {
+            "id": "c3-1",
+            "name": "Alex",
+            "image": null
+          },
+          {
+            "id": "c3-2",
+            "name": "Wangui",
+            "image": null
+          },
+          {
+            "id": "p-pt9az6q",
+            "name": "Nyambura",
+            "image": null
+          }
+        ]
+      },
+      {
+        "id": "c4",
+        "name": "Charles Ngumi",
+        "image": null,
+        "children": [
+          {
+            "id": "c4-1",
+            "name": "Josiah",
+            "image": null
+          },
+          {
+            "id": "p-xcatjr6",
+            "name": "Juma",
+            "image": null
+          }
+        ]
+      },
+      {
+        "id": "c5",
+        "name": "Simon Njuiri",
+        "image": null,
+        "children": [
+          {
+            "id": "c5-1",
+            "name": "Lorna",
+            "image": null
+          },
+          {
+            "id": "p-hsjq7s1",
+            "name": "Nora",
+            "image": null
+          },
+          {
+            "id": "p-r0kci4s",
+            "name": "Wangari",
+            "image": null
+          }
+        ]
+      },
+      {
+        "id": "c6",
+        "name": "Samuel Gichanga",
+        "image": null,
+        "children": [
+          {
+            "id": "c6-1",
+            "name": "Trevor",
+            "image": null
+          },
+          {
+            "id": "p-7lf4vra",
+            "name": "Tracy",
+            "image": null
+          }
+        ]
+      },
+      {
+        "id": "c7",
+        "name": "Racheal Wairimu Musee",
+        "image": null,
+        "children": [
+          {
+            "id": "c7-1",
+            "name": "Mwende",
+            "image": null
+          },
+          {
+            "id": "p-4vgfxsx",
+            "name": "Monicah",
+            "image": null
+          },
+          {
+            "id": "p-td07tvi",
+            "name": "Angela",
+            "image": null
+          }
+        ]
+      },
+      {
+        "id": "c8",
+        "name": "Catherine Muthoni",
+        "image": null,
+        "children": [
+          {
+            "id": "c8-1",
+            "name": "Hilda",
+            "image": null
+          },
+          {
+            "id": "p-ylduywq",
+            "name": "Don",
+            "image": null
+          }
+        ]
+      },
+      {
+        "id": "c9",
+        "name": "Stephen Thumi",
+        "image": null,
+        "children": [
+          {
+            "id": "c9-1",
+            "name": "Wamutu",
+            "image": null
+          },
+          {
+            "id": "p-9w3auoy",
+            "name": "Wangui",
+            "image": null
+          }
+        ]
+      },
+      {
+        "id": "c10",
+        "name": "Rose Wambui",
+        "image": null,
+        "children": [
+          {
+            "id": "c10-1",
+            "name": "Stacy",
+            "image": null
+          }
+        ]
+      }
+    ]
+  };
